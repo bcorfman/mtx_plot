86fefc02dc2b11b09ca914cf2129c8c3ed035813
bcorfman <bcorfman@fastmail.fm>
2017-05-23 23:06:32 -0400

    added gridlines and offsets to matrix borders

--- plotter.py
+++ plotter.py
@@ -19,10 +19,14 @@ class ExcelPlot:
                 for row, item in enumerate(mtx.header_lines):
                     ws.write(row, 0, item)
                     pk_row = row
+                for d in range(mtx.cls_defl):
+                    ws.write(pk_row + 1, d, mtx.gridlines_defl[d])
+                    ws.write(pk_row + 2, d, mtx.gridlines_defl[d] + mtx.offset_defl)
                 for r in range(mtx.cls_range):
+                    ws.write(pk_row + r + 3, 0, mtx.gridlines_range[r])
+                    ws.write(pk_row + r + 3, 1, mtx.gridlines_range[r] + mtx.offset_range)
                     for d in range(mtx.cls_defl):
-                        ws.write(pk_row + r, d, mtx.pks[r, d])
-                ws.conditional_format(pk_row, 0, pk_row + mtx.cls_range, mtx.cls_defl,  {'type': '3_color_scale',
-                                                                                         'min_color': '#63BE7B',
-                                                                                         'mid_color': '#F7B97B',
-                                                                                         'max_color': '#EF676A'})
+                        ws.write(pk_row + r + 3, d + 2, mtx.pks[r, d])
+                ws.conditional_format(pk_row + 3, 2, pk_row + mtx.cls_range + 3, mtx.cls_defl + 2,
+                                      {'type': '3_color_scale', 'min_color': '#63BE7B', 'mid_color': '#F7B97B',
+                                       'max_color': '#EF676A'})
